5910033b799df674787d18b2f8fd81e848442e6d
Harkirat Singh <hs3188248@gmail.com>
2022-12-04 22:39:02 +0530

    sql database file

---
+++ student_database.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE student;
+
+-- create  ==>command data defination language 
+-- student is the name of the database (It could be anything)
+-- integer ==> these are the numbers from 0 to infinity
+
+
+use student;
+
+CREATE TABLE student_details(
+student_rollno int,
+student_name char(50)
+);
+
+INSERT INTO student_details()
+VALUES(
+'1904999',
+'Harkirat'
+)
+
+
